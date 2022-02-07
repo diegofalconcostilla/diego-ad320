32710451e749c196a0661f620eaa65692342f83e
diegofalconc <diegofalconcostilla94@gmail.com>
2022-02-06 16:42:47 -0800

    Initial commit and base design

---
+++ client/src/components/FlashCard/FlashCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './FlashCard.css'
+
+function FlashCard() {
+    return (
+        <div className="card">
+            <img src='http://placekitten.com/200/300' />
+            <p>
+                random content
+            </p>
+        </div>
+    )
+}
+
+export default FlashCard;
