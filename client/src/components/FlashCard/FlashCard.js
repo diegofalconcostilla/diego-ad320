08f723cf776e665ef827ab4168d3ebdfd6b64f3a
diegofalconc <diegofalconcostilla94@gmail.com>
2022-02-08 16:43:54 -0800

    fixed cat resolution and added Lorem Ipsum fill text

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -4,9 +4,9 @@ import './FlashCard.css'
 function FlashCard() {
     return (
         <div className="card">
-            <img src='http://placekitten.com/200/300' />
+            <img src='http://placekitten.com/200/200' />
             <p>
-                random content
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ."
             </p>
         </div>
     )
