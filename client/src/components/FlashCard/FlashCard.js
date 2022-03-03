c13b69a64ebf53e7c6919eb1c67b7e0f7fe67ade
diegofalconc <diegofalconcostilla94@gmail.com>
2022-03-02 23:09:18 -0800

    flashcard changes

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -1,15 +1,47 @@
-import React from "react";
-import './FlashCard.css'
+import React from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@mui/material'
 
-function FlashCard() {
-    return (
-        <div className="card">
-            <img src='http://placekitten.com/200/200' alt=""/>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate .
-            </p>
-        </div>
-    )
+function Flashcard({ content, previous, next, flip }) {
+  return (
+    <Box sx={{ width: '100%', mt: 8, display: 'flex', justifyContent: 'space-around'}}>
+      <Card sx={{ width: '40vw' }} elevation={3}>
+        { content.image && <CardMedia
+          component="img"
+          height="200"
+          image={content.image} />}
+        <CardContent>
+          <Typography>{content.text ?? ''}</Typography>
+        </CardContent>
+        <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Button
+            sx={{ mx: 2 }}
+            onClick={ () => previous() }
+            variant="outlined"
+          >
+            Prev
+          </Button>
+          <Button sx={{ mx: 2 }} variant="outlined" onClick={() => flip()}>
+            Flip
+          </Button>
+          <Button
+            sx={{ mx: 2 }}
+            onClick={ () => next() }
+            variant="outlined"
+          >
+            Next
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  )
 }
 
-export default FlashCard;
+export default Flashcard
