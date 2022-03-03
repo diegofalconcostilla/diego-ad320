413b9c71f95f0569fdec438b052ec635d643004a
diegofalconc <diegofalconcostilla94@gmail.com>
2022-03-02 23:03:52 -0800

    week 8 initial submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import './App.css';
-import Topbar from './components/Topbar/Topbar';
-import CardNavigation from './components/CardNavigation/CardNavigation';
-import FlashCard from './components/FlashCard/FlashCard';
-import Button from './components/Button/Button';
+import React, { useEffect, useState } from 'react'
+import Container from '@mui/material/Container'
+import './App.css'
+import Topbar from './components/Topbar/Topbar'
+import DeckProvider from './components/Deck/DeckProvider'
+import axios from 'axios'
 
-const controls = ["Back", "Flip", "Next"];
 function App() {
+  const [createMode, setCreateMode] = useState(false)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/users').then((response) => {
+      console.log(`response from users ${response.data[0].firstName}`)
+      setUser(response.data[0])
+    })
+  }, [])
+
   return (
     <React.Fragment>
-      <Topbar />
-      <div className="container">
-        <CardNavigation />
-        <div className="card-container">
-          <FlashCard />
-          <div className="card-controls">
-            {controls.map((control) => {
-              return <Button>{control}</Button>
-            })}
-          </div>
-        </div>
-      </div>
+      <Topbar createMode={createMode} createCardHandler={() => { setCreateMode(!createMode) }} />
+      <Container width="lg">
+        {user === null ? <span>Loading...</span> :
+          <DeckProvider userId={user._id} decks={user.decks} createMode={createMode} /> }
+      </Container>
     </React.Fragment>
-  );
+  )
 }
 
 export default App;
